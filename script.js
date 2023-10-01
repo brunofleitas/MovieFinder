cbe61e21ada9147aebf666d7c526b4e5d7259e86
brunofleitas <brunofleitas2011@gmail.com>
2023-10-01 15:37:21 +0200

    Se creo pagina usando API

---
+++ script.js
@@ -0,0 +1,34 @@
+const apiKey = 'tu_api_key_aqui';
+
+function searchMovies() {
+    const searchInput = document.getElementById('searchInput');
+    const searchTerm = searchInput.value;
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultsDiv = document.getElementById('results');
+            resultsDiv.innerHTML = '';
+
+            data.results.forEach(movie => {
+                const movieDiv = document.createElement('div');
+                movieDiv.classList.add('movie');
+
+                const title = document.createElement('h2');
+                title.textContent = movie.title;
+
+                const overview = document.createElement('p');
+                overview.textContent = movie.overview;
+
+                const rating = document.createElement('p');
+                rating.textContent = `Calificación: ${movie.vote_average}`;
+
+                movieDiv.appendChild(title);
+                movieDiv.appendChild(overview);
+                movieDiv.appendChild(rating);
+
+                resultsDiv.appendChild(movieDiv);
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
